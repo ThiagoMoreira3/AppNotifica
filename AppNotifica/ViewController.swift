5d4156f4de241c2e3feb86c844c886f25b49df77
Thiago Moreira Rocha <138623506+ThiagoMoreira3@users.noreply.github.com>
2023-11-08 11:07:12 -0300

    Initial Commit

---
+++ AppNotifica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNotifica
+//
+//  Created by IFB-BIOTIC-21 on 08/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
